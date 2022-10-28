c33d5aad5417eb1bcb49319048781807adb0c32c
Sheetal1614 <sehrawats1614@gmail.com>
2022-10-28 12:48:02 +0530

    Create category integration test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,9 +10,9 @@ class ApplicationController < ActionController::Base
 	end
 
 	def require_user
-
-    if current_user != @article.user && !current_user.admin?      		flash[:alert] = "You must be logged in to perform that action"
-      		redirect_to login_path
+		if !logged_in?
+	    	flash[:alert] = "You must be logged in to perform that action"
+	      	redirect_to login_path
     	end
   	end
 end
